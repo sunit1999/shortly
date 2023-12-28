97477e3013e9b32f9fa9d735826652e4d054a921
sunit <sunitsharma1999@gmail.com>
2023-12-28 20:15:26 +0530

    removed redundant code

--- src/main/kotlin/com/sunit/demo/repository/PersonRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.sunit.demo.repository
-
-import com.sunit.demo.models.Person
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PersonRepository: JpaRepository<Person, Long>
